3a259bae7e0083aaae9fb4d5c6a5168f52edbb45
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2022-05-11 18:31:52 +0600

    alternating between players

--- connect4.py
+++ connect4.py
@@ -7,4 +7,21 @@ def create_board():
 
 
 board = create_board()
-print(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for player 1 input
+    if turn == 0:
+        selection = input("Player 1 Make your selection (0-6)")
+        selection = int(selection)
+
+        print(selection)
+
+    # Ask for player 2 input
+    else:
+        selection = input("Player 2 Make your selection (0-6)")
+        selection = int(selection)
+
+    turn += 1
+    turn = turn % 2
